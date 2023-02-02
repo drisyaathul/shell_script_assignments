cfcbda2eb74c13e07690f56b21b200b4f5d26886
drisyaathul <drisyak809@gmail.com>
2023-02-03 01:16:14 +0800

    Unit conversion by using Case

--- day5/case_unit_conversion.sh
+++ day5/case_unit_conversion.sh
@@ -1,10 +1,11 @@
-
+$
 #Write a program that takes User Inputs and does Unit Conversion of different Length units :
 
 #1. Feet to Inch		2. Feet to Meter		3. Inch to Feet		4. Meter to Feet
 
 
 #!/bin/bash
+
 
 read -p "Enter a number : " number;
 echo "1) Feet to inch conversion  2) Feet to meter conversion  3) Inch to feet conversion  4) Meter to feet conversion";
@@ -22,8 +23,8 @@ case $choice in
 	echo "$number feet to meter = "$result;
 ;;
 3)
-	result=$(($number/12));
-	echo "$number inch to feet  = "$result;
+	result=`echo $number | awk '{print $number/12}'`
+	echo "$number Inch to feet = "$result;
 ;;
 4)
 	result=`echo $number | awk '{print $number/0.3048}'`;
