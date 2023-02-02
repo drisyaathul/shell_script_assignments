7a2f40d6e331f0d2affaffb9cac3fe84ea8b926e
drisyaathul <drisyak809@gmail.com>
2023-02-03 02:06:40 +0800

    Power of 2 that are less than or equal to 2^n

---
+++ day6/power_of_2.sh
@@ -0,0 +1,16 @@
+#Write a program that takes a command-line argument n and prints a table of the
+#powers of 2 that are less than or equal to 2^n.
+
+#!/bin/bash
+
+
+
+for ((i=1; i<=1; i++));
+
+
+do
+
+	echo $((2**$i));
+
+
+done
