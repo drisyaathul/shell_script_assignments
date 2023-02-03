9b1ef3f2d238a0a5fe757ec62146a07d9ea7e0f8
drisyaathul <drisyak809@gmail.com>
2023-02-03 11:21:41 +0800

    Find the Prime Number

---
+++ day6/prime_number.sh
@@ -0,0 +1,29 @@
+#Write a program that takes a input and determines if the number is a prime:
+
+
+#!/bin/bash
+
+
+factors=0
+
+read -p "Enter the number: " n
+
+for((i=2; i<=$n-1; i++));
+
+
+
+do
+		if [ $(($n%$i)) -eq 0 ];
+		then
+                factors+=1
+
+        	elif [ $factors -eq 0 ];
+		then
+        		echo $n "is a prime number."
+
+		else
+        		echo $n "is not a prime number."
+
+		fi
+
+done
